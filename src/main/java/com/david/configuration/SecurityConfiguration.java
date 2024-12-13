45c83c0de567df45a5bbc4dec893ba10b24c7231
davidchen <david82810@gmail.com>
2024-12-13 17:05:09 +0800

    init resource server

--- src/main/java/com/david/configuration/SecurityConfiguration.java
+++ src/main/java/com/david/configuration/SecurityConfiguration.java
@@ -1,4 +1,22 @@
 package com.david.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests(request ->
+                        request.requestMatchers("/public").permitAll()
+                                .anyRequest().authenticated())
+                .oauth2ResourceServer(server -> server.jwt(Customizer.withDefaults()))
+                .build();
+    }
 }
--- src/main/java/com/david/controller/TestController.java
+++ src/main/java/com/david/controller/TestController.java
@@ -1,4 +1,31 @@
 package com.david.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@Slf4j
 public class TestController {
+    @GetMapping("/test")
+    public Map<String, String> test(Authentication authentication) {
+        log.info("authentication:{}", authentication);
+        Jwt jwt = (Jwt) authentication.getPrincipal();
+        String username = jwt.getClaimAsString("sub");
+        log.info("username:{}", username);
+
+        return Map.of("message", "Hello World", "status", "ok");
+    }
+
+    @PreAuthorize("hasAuthority('SCOPE_write')")
+    @GetMapping("/test2")
+    public String tes2() {
+        return "Hello World";
+    }
+
 }
